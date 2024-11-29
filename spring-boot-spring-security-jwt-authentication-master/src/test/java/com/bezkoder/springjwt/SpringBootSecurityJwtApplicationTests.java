0f48077dfbd7bcb0cbd8f7206e3a9417395e1e04
RajkumarGapat <rajkumargapat345@gmail.com>
2024-11-29 15:25:09 +0530

    VRV Assignment

---
+++ spring-boot-spring-security-jwt-authentication-master/src/test/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bezkoder.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootSecurityJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	@Test
+	public void addTest() {
+	}
+
+}
